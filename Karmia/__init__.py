b8df51008ad85737f371b31fb47989fa003f4527
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2016-10-21 12:51:07 +0900

    Initial commit

---
+++ Karmia/__init__.py
@@ -0,0 +1,13 @@
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
+
+
+from Karmia import context
+
+
+
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# c-hanging-comment-ender-p: nil
+# End:
---
+++ Karmia/context.py
@@ -0,0 +1,82 @@
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
+
+
+import inspect
+
+
+class KarmiaContext:
+    def __init__(self, options = None):
+        options = options if options else {}
+        callback = options.get('callback', ['callback'])
+        self.callback = callback if isinstance(callback, list) else [callback]
+        self.parameters = {}
+
+
+    def set(self, key, value=None):
+        parameters = {}
+
+        if isinstance(key, dict):
+            parameters = key
+        else:
+            parameters[key] = value
+
+        self.parameters.update(parameters)
+
+        return self
+
+
+    def get(self, key, default_value=None):
+        return self.parameters.get(key, default_value)
+
+
+    def remove(self, key):
+        if key in self.parameters:
+            self.parameters.pop(key)
+
+        return self
+
+
+    def child(self):
+        child = KarmiaContext()
+        parameters = {}
+        parameters.update(self.parameters)
+
+        child.parameters = parameters
+
+        return child
+
+
+    def annotate(self, fn):
+        return inspect.signature(fn).parameters
+
+
+    def invoke(self, fn, parameters=None):
+        parameters = parameters if parameters else {}
+        parameters['context'] = parameters.get('context', self)
+        values = map(lambda key: parameters.get(key, None), self.annotate(fn))
+
+        return fn(*values)
+
+
+    def call(self, fn, parameters=None, callback=None):
+        parameters = parameters if parameters else {}
+        if callable(parameters):
+            callback = parameters
+            parameters = {}
+
+        values = {}
+        values.update(self.parameters)
+        values.update(parameters)
+        if callback:
+            values.update({key: callback for key in self.callback})
+
+        return self.invoke(fn, values)
+
+
+
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# c-hanging-comment-ender-p: nil
+# End:
